0593c5d02914c9566f5c132434b2f99f59f1af4c
josedom24 <josedom24@gmail.com>
2017-05-11 14:55:57 +0200

    Correción de wsgi

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ import bottle
 from bottle import route, run, template
 
 @route('/')
-def index(name):
+def index():
     return '<b>Hello word!!!</b>'
 
 if __name__ == "__main__":
 	run(host='0.0.0.0', port=8080, server='gunicorn', workers=4)
 
-app = bottle.default_app()
 
