1ff0ace86194819ef1199e4c6a3272e81223a599
José Domingo Muñoz Rodríguez <josedom24@gmail.com>
2018-09-25 17:50:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from bottle import route, run, template
 
 @route('/')
 def index():
-    return '<b>Hello word!!!</b>'
+    return '<b>Hello word 2!!!</b>'
 
 if __name__ == "__main__":
 	run(host='0.0.0.0', port=8080, server='gunicorn', workers=4)
